55c5e462ab92dfcbb5c3fabb2e8a8ac523c44369
wheelermt <matthew.t.wheeler@uea.ac.uk>
2023-04-04 20:46:30 +0100

    BEC-3 Added 3D grid class

--- include/grid.h
+++ include/grid.h
@@ -1,6 +1,7 @@
 #ifndef BECPP_GRID_H
 #define BECPP_GRID_H
 
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -85,4 +86,56 @@ public:
     friend class Wavefunction;
 };
 
-#endif //BECPP_GRID_H
+class Grid3D : public Grid
+{
+private:
+    void constructGridParams() override;
+    void constructGrids() override;
+    void fftshift() override;
+
+    const unsigned int m_xPoints{};
+    const unsigned int m_yPoints{};
+    const unsigned int m_zPoints{};
+    double m_xGridSpacing{};
+    double m_yGridSpacing{};
+    double m_zGridSpacing{};
+    double m_gridSpacingProduct{};
+    double m_xFourierGridSpacing{};
+    double m_yFourierGridSpacing{};
+    double m_zFourierGridSpacing{};
+    double m_xLength{};
+    double m_yLength{};
+    double m_zLength{};
+
+    std::vector<doubleArray_t> m_xMesh{};
+    std::vector<doubleArray_t> m_yMesh{};
+    std::vector<doubleArray_t> m_zMesh{};
+    std::vector<doubleArray_t> m_xFourierMesh{};
+    std::vector<doubleArray_t> m_yFourierMesh{};
+    std::vector<doubleArray_t> m_zFourierMesh{};
+    std::vector<doubleArray_t> m_wavenumber{};
+
+public:
+    Grid3D(std::tuple<unsigned int, unsigned int, unsigned int> points,
+           std::tuple<double, double, double> gridSpacing);
+    ~Grid3D() override = default;
+
+    [[nodiscard]] unsigned int xPoints() const;
+    [[nodiscard]] unsigned int yPoints() const;
+    [[nodiscard]] unsigned int zPoints() const;
+    [[nodiscard]] double xGridSpacing() const;
+    [[nodiscard]] double yGridSpacing() const;
+    [[nodiscard]] double zGridSpacing() const;
+    [[nodiscard]] double gridSpacingProduct() const;
+    [[nodiscard]] double xFourierGridSpacing() const;
+    [[nodiscard]] double yFourierGridSpacing() const;
+    [[nodiscard]] double zFourierGridSpacing() const;
+    [[nodiscard]] double xLength() const;
+    [[nodiscard]] double yLength() const;
+    [[nodiscard]] double zLength() const;
+    [[nodiscard]] std::vector<doubleArray_t> wavenumber() const;
+
+    friend class Wavefunction;
+};
+
+#endif//BECPP_GRID_H
--- src/grid.cpp
+++ src/grid.cpp
@@ -158,3 +158,100 @@ double Grid1D::xGridSpacing() const { return m_xGridSpacing; }
 double Grid1D::xFourierGridSpacing() const { return m_xFourierGridSpacing; }
 double Grid1D::xLength() const { return m_xLength; }
 std::vector<double> Grid1D::wavenumber() const { return m_wavenumber; }
+
+void Grid3D::constructGridParams()
+{
+    m_gridSpacingProduct = m_xGridSpacing * m_yGridSpacing * m_zGridSpacing;
+
+    m_xFourierGridSpacing = PI / (m_xPoints / 2. * m_xGridSpacing);
+    m_yFourierGridSpacing = PI / (m_yPoints / 2. * m_yGridSpacing);
+    m_zFourierGridSpacing = PI / (m_zPoints / 2. * m_zGridSpacing);
+
+    m_xLength = m_xPoints * m_xGridSpacing;
+    m_yLength = m_yPoints * m_yGridSpacing;
+    m_zLength = m_zPoints * m_zGridSpacing;
+}
+
+void Grid3D::constructGrids()
+{
+    // Set grid sizes
+    m_xMesh.resize(m_xPoints,
+                   std::vector<std::vector<double>>(
+                           m_yPoints, std::vector<double>(m_zPoints)));
+    m_yMesh.resize(m_xPoints,
+                   std::vector<std::vector<double>>(
+                           m_yPoints, std::vector<double>(m_zPoints)));
+    m_xFourierMesh.resize(m_xPoints,
+                          std::vector<std::vector<double>>(
+                                  m_yPoints, std::vector<double>(m_zPoints)));
+    m_yFourierMesh.resize(m_xPoints,
+                          std::vector<std::vector<double>>(
+                                  m_yPoints, std::vector<double>(m_zPoints)));
+    m_wavenumber.resize(m_xPoints,
+                        std::vector<std::vector<double>>(
+                                m_yPoints, std::vector<double>(m_zPoints)));
+
+    // Construct grids
+    for (int i = 0; i < m_xPoints; ++i)
+    {
+        for (int j = 0; j < m_yPoints; ++j)
+        {
+            for (int k = 0; k < m_zPoints; ++k)
+            {
+                m_xMesh[i][j][k] = (j - m_xPoints / 2.) * m_xGridSpacing;
+                m_xFourierMesh[i][j][k] =
+                        (j - m_xPoints / 2.) * m_xFourierGridSpacing;
+                m_yMesh[j][i][k] = (j - m_yPoints / 2.) * m_yGridSpacing;
+                m_yFourierMesh[j][i][k] =
+                        (j - m_yPoints / 2.) * m_yFourierGridSpacing;
+            }
+        }
+    }
+
+    // Shift the k-space grids, so they are in the right order
+    fftshift();
+}
+
+void Grid3D::fftshift()
+{
+    // TODO: Implement using std::rotate
+}
+
+Grid3D::Grid3D(std::tuple<unsigned int, unsigned int, unsigned int> points,
+               std::tuple<double, double, double> gridSpacing)
+    : m_xPoints{std::get<0>(points)}, m_yPoints{std::get<1>(points)},
+      m_zPoints{std::get<2>(points)}, m_xGridSpacing{std::get<0>(gridSpacing)},
+      m_yGridSpacing{std::get<1>(gridSpacing)},
+      m_zGridSpacing{std::get<2>(gridSpacing)}
+{
+    Grid3D::constructGridParams();
+    Grid3D::constructGrids();
+}
+
+unsigned int Grid3D::xPoints() const { return m_xPoints; }
+
+unsigned int Grid3D::yPoints() const { return m_yPoints; }
+
+unsigned int Grid3D::zPoints() const { return m_zPoints; }
+
+double Grid3D::xGridSpacing() const { return m_xGridSpacing; }
+
+double Grid3D::yGridSpacing() const { return m_yGridSpacing; }
+
+double Grid3D::zGridSpacing() const { return m_zGridSpacing; }
+
+double Grid3D::gridSpacingProduct() const { return m_gridSpacingProduct; }
+
+double Grid3D::xFourierGridSpacing() const { return m_xFourierGridSpacing; }
+
+double Grid3D::yFourierGridSpacing() const { return m_yFourierGridSpacing; }
+
+double Grid3D::zFourierGridSpacing() const { return m_zFourierGridSpacing; }
+
+double Grid3D::xLength() const { return m_xLength; }
+
+double Grid3D::yLength() const { return m_yLength; }
+
+double Grid3D::zLength() const { return m_zLength; }
+
+std::vector<doubleArray_t> Grid3D::wavenumber() const { return m_wavenumber; }
