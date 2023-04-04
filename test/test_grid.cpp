9757adf5da0d31815c72e95bb4ea4e3156215b03
wheelermt <matthew.t.wheeler@uea.ac.uk>
2023-04-04 20:45:41 +0100

    BEC-3 Added grid tests

--- test/test_grid.cpp
+++ test/test_grid.cpp
@@ -68,3 +68,44 @@ TEST_F(Grid2DTest, WavenumberSetCorrectly)
 {
     ASSERT_EQ(grid.wavenumber()[0][0], 0.0);
 }
+
+class Grid3DTest : public ::testing::Test
+{
+public:
+    std::tuple<unsigned int, unsigned int, unsigned int> points{64, 64, 64};
+    std::tuple<double, double, double> gridSpacings{0.5, 0.5, 0.5};
+    Grid3D grid{points, gridSpacings};
+};
+
+TEST_F(Grid3DTest, PointsSetCorrectly)
+{
+    ASSERT_EQ(grid.xPoints(), 64);
+    ASSERT_EQ(grid.yPoints(), 64);
+    ASSERT_EQ(grid.zPoints(), 64);
+}
+
+TEST_F(Grid3DTest, SpacingSetCorrectly)
+{
+    ASSERT_EQ(grid.xGridSpacing(), 0.5);
+    ASSERT_EQ(grid.yGridSpacing(), 0.5);
+    ASSERT_EQ(grid.zGridSpacing(), 0.5);
+}
+
+TEST_F(Grid3DTest, SpacingProductCorrect)
+{
+    ASSERT_EQ(grid.gridSpacingProduct(), 0.5 * 0.5 * 0.5);
+}
+
+TEST_F(Grid3DTest, FourierSpacingSetCorrectly)
+{
+    ASSERT_EQ(grid.xFourierGridSpacing(), PI / 16.0);
+    ASSERT_EQ(grid.yFourierGridSpacing(), PI / 16.0);
+    ASSERT_EQ(grid.zFourierGridSpacing(), PI / 16.0);
+}
+
+TEST_F(Grid3DTest, LengthSetCorrectly)
+{
+    ASSERT_EQ(grid.xLength(), 32);
+    ASSERT_EQ(grid.yLength(), 32);
+    ASSERT_EQ(grid.zLength(), 32);
+}
